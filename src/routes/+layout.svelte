da06f0097b2b57ca37e3cdb5aa3450a184cf411b
Florian Schmidt <flo.schmidt10@gmail.com>
2026-02-13 22:33:10 +0100

    fix layout, finalize(?) login

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import CampusTitleBar from '$lib/components/page/CampusTitleBar.svelte';
+	import { page } from '$app/state';
 
 	let { children } = $props();
 </script>
@@ -10,11 +11,20 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="flex min-h-dvh flex-col">
+{#if page.route.id === '/'}
+	<div class="flex min-h-dvh flex-col">
+		<div class="flex min-h-dvh flex-col">
+			<CampusTitleBar />
+			<main class="flex flex-1 flex-col justify-center">
+				{@render children?.()}
+			</main>
+		</div>
+	</div>
+{:else}
 	<div class="flex min-h-dvh flex-col">
 		<CampusTitleBar />
-		<main class="flex flex-1 flex-col justify-center">
-			{@render children?.()}
+		<main class="flex-1 space-y-6 p-4 pt-6">
+			{@render children()}
 		</main>
 	</div>
-</div>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -68,17 +68,18 @@
 					/>
 				</div>
 
-				<div class="rounded-box bg-base-200 p-3">
-					<label class="label cursor-pointer items-start justify-start gap-3">
+				<div class=" space-y-2 rounded-2xl bg-base-100 p-3 shadow-lg">
+					<label class="label w-full cursor-pointer items-start justify-start gap-3">
 						<input
 							type="checkbox"
 							name="dataConsent"
 							required
-							class="checkbox mt-1 checkbox-primary"
+							class="checkbox mt-1 shrink-0 checkbox-primary"
 						/>
-						<span class="label-text">
-							Ich stimme zu, dass sich <span
-								class="bg-linear-to-br from-blue-700 to-pink-600 box-decoration-clone bg-clip-text text-transparent"
+						<span class="label-text flex-1 leading-snug whitespace-normal">
+							Ich stimme zu, dass sich
+							<span
+								class="bg-linear-to-br from-primary to-accent box-decoration-clone bg-clip-text text-transparent"
 								>CampusUnbloat</span
 							>
 							in meinem Namen bei CampusDual einloggt.
@@ -86,9 +87,10 @@
 						</span>
 					</label>
 
-					<div class="mt-2 flex justify-between text-sm">
-						<a class="link link-hover" href={resolve('/impressum')}>Impressum</a>
-						<a class="link link-hover" href={resolve('/datenschutz')}>Datenschutz</a>
+					<div class="mt-2 flex justify-end space-x-2">
+						<a class="btn btn-soft btn-sm btn-primary" href={resolve('/impressum')}>Impressum</a>
+						<a class="btn btn-soft btn-sm btn-primary" href={resolve('/datenschutz')}>Datenschutz</a
+						>
 					</div>
 				</div>
 
@@ -96,7 +98,6 @@
 					<button
 						type="submit"
 						class="btn w-full btn-primary"
-						class:loading={loginInProgress}
 						disabled={loginInProgress}
 						aria-busy={loginInProgress}
 					>
