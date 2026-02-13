b820ee70586dd494b46fa4b16c6c62f14a7f629a
Florian Schmidt <flo.schmidt10@gmail.com>
2026-02-13 20:56:40 +0100

    wip

---
+++ src/lib/components/page/CampusTitleBar.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+	import { LogOut } from '@lucide/svelte';
+
+	// import Logo from '$lib/assets/logo-kiel-sailing-city.svelte';
+</script>
+
+<!-- Obere Navigationsleiste; Padding oben respektiert Safe-Area-Inset für nicht-eckige Bildschirme -->
+<nav
+	class="sticky top-0 z-50 flex h-14 items-center justify-between
+	border-b border-b-base-300 bg-base-200 px-4 pt-[calc(1rem+env(safe-area-inset-top,0px))] pb-4 select-none"
+>
+	<!-- <div class="w-24 dark:brightness-90 dark:invert dark:saturate-0">
+		<Logo />
+	</div> -->
+	<p class="text-xl font-bold">CampusUnbloat</p>
+	<div class="flex flex-row space-x-1">
+		<div class="dropdown dropdown-center">
+			<div tabindex="0" role="button" class="btn my-1 btn-circle btn-sm">
+				<p class="font-mono text-[14px]">i</p>
+			</div>
+			<div tabindex="-1" class="dropdown-content card z-1 w-64 bg-base-100 shadow-md card-sm">
+				<div class="card-body">
+					<a href={resolve('/')} class="btn">Impressum</a>
+					<a href={resolve('/')} class="btn">Datenschutz</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="dropdown dropdown-center">
+			<div tabindex="0" role="button" class="btn my-1 rounded-full btn-sm">Rechtliches</div>
+			<div tabindex="-1" class="dropdown-content card z-1 w-64 bg-base-100 shadow-md card-sm">
+				<div class="card-body">
+					<a href={resolve('/')} class="btn">Impressum</a>
+					<a href={resolve('/')} class="btn">Datenschutz</a>
+				</div>
+			</div>
+		</div>
+
+		<button class="btn my-1 rounded-full btn-sm"><LogOut size={16} strokeWidth={3} /></button>
+	</div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import CampusTitleBar from '$lib/components/page/CampusTitleBar.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="flex min-h-dvh flex-col">
+	<CampusTitleBar />
+	<main class="flex-1 space-y-6 p-4 pt-6">
+		{@render children()}
+	</main>
+</div>
