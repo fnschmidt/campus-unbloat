4f18359e19be776558b7147cda788e01cce7903a
Florian Schmidt <flo.schmidt10@gmail.com>
2026-02-26 14:57:54 +0100

    mensatile done ig

--- src/lib/TilesAndModals/MensaTile.svelte
+++ src/lib/TilesAndModals/MensaTile.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import DashboardTile from '$lib/DashboardTile.svelte';
+	import { faUtensils } from '@fortawesome/free-solid-svg-icons';
 	import MensaModal from './MensaModal.svelte';
+	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
 
 	let modal: HTMLDialogElement | null = $state(null);
 </script>
@@ -8,5 +10,15 @@
 <MensaModal bind:modal></MensaModal>
 
 <DashboardTile title="Mensa" on:click={() => modal?.showModal()} ready={Boolean(modal)}>
-	click me
+	<div class="flex flex-col items-center gap-3">
+		<div class="stack h-20 w-28">
+			<div class="grid place-content-center rounded-box bg-primary text-primary-content">
+				<FontAwesomeIcon size="xl" icon={faUtensils} />
+			</div>
+			<div class="rounded-box bg-primary"></div>
+			<div class="rounded-box bg-primary"></div>
+		</div>
+
+		Tippe, um Gerichte anzuzeigen
+	</div>
 </DashboardTile>
